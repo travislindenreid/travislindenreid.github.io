83757f41e6290c508ec658fdd424b37ff693d9ff
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2026-08-16 23:50:34 -0700

    fixed background gradient not reaching image

---
+++ scripts/decor.js
@@ -0,0 +1,19 @@
+window.onload = handleOnload;
+
+function handleOnload () {
+    // it's ok if this is an estimate. we just need to lengthen the top color of the gradient
+    // to be much longer if the page is long so that the gradient doesn't go transparent
+    // before overlapping the background image
+    var pageHeight = document.body.scrollHeight;
+    var bgImgHeight = 0.5625 * document.body.clientWidth;
+    var percentPageToTopOfBgImg = bgImgHeight/pageHeight * 100;
+    console.log(percentPageToTopOfBgImg);
+
+    var bg = "linear-gradient(0deg, rgba(156, 217, 65, 0.5) 0%, rgba(0, 0, 0, 1) " + percentPageToTopOfBgImg + "%, rgba(0, 0, 0, 1) 100%), url(\"assets/grid.jpg\")";
+    document.body.style.background = bg;
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundPosition = "center bottom";
+    document.body.style.backgroundRepeat = "no-repeat";
+    console.log(bg);
+    console.log(document.body.style.background);
+}
